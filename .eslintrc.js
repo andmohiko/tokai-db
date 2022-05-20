a9a45d81e5ef2e908a0a459a83b9303dff9a0f44
andmohiko <andmohiko@gmail.com>
2022-05-21 07:18:24 +0900

    chore: add ts eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,60 +3,57 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'react', 'import'],
+  plugins: ["@typescript-eslint", "react", "import"],
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
       },
       typescript: {
-        config: 'tsconfig.json',
-        alwaysTryTypes: true
-      }
-    }
+        config: "tsconfig.json",
+        alwaysTryTypes: true,
+      },
+    },
   },
   rules: {
-    '@typescript-eslint/ban-types': [
-      'error',
+    "@typescript-eslint/ban-types": [
+      "error",
       {
         types: {
-          '{}': false
-        }
-      }
+          "{}": false,
+        },
+      },
     ],
-    'react/prop-types': ['off'],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'import/order': ['error'],
-    'prettier/prettier': [
-      'error',
+    "react/prop-types": ["off"],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "import/order": ["error"],
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'none',
-        endOfLine: 'lf',
+        trailingComma: "none",
+        endOfLine: "lf",
         semi: false,
         singleQuote: true,
         printWidth: 80,
-        tabWidth: 2
-      }
-    ]
-  }
-}
+        tabWidth: 2,
+      },
+    ],
+  },
+};
