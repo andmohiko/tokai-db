346be7bdd41e8f0d21d4f5d23f89cf5982bb0175
andmohiko <andmohiko@gmail.com>
2022-06-28 01:40:15 +0900

    feat: tagsSelector component

---
+++ src/components/TagButton.tsx
@@ -0,0 +1,62 @@
+import { Tag } from '@chakra-ui/react'
+import { TagUI } from '@/entities'
+
+interface TagButtonProps {
+  tag: TagUI
+  selectTag: any
+}
+
+export const TagButton = ({
+  tag,
+  selectTag
+}: TagButtonProps): React.ReactElement => {
+  return (
+    <button onClick={() => selectTag(tag)} type="button">
+      <TagLabel tagLabel={tag.label} isActive={tag.isActive} />
+    </button>
+  )
+}
+
+interface TagLabelProps {
+  tagLabel: string
+  isActive?: boolean
+}
+
+export const TagLabel = ({
+  tagLabel,
+  isActive = false
+}: TagLabelProps): React.ReactElement => {
+  const focusedColor = 'red'
+  let tagColor = 'gray'
+  switch (tagLabel) {
+    case 'てつや':
+      tagColor = 'orange'
+      break
+    case 'しばゆー':
+      tagColor = 'yellow'
+      break
+    case 'りょう':
+      tagColor = 'blue'
+      break
+    case 'としみつ':
+      tagColor = 'green'
+      break
+    case 'ゆめまる':
+      tagColor = 'pink'
+      break
+    case '虫眼鏡':
+      tagColor = 'red'
+      break
+    default:
+      tagColor = 'gray'
+  }
+
+  return (
+    <Tag
+      colorScheme={isActive ? focusedColor : tagColor}
+      variant={isActive ? 'solid' : 'subtle'}
+    >
+      {tagLabel}
+    </Tag>
+  )
+}
