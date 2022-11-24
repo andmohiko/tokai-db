ff4adc132c921d45367f7a94eb2ce754e9973163
andmohiko <andmohiko@gmail.com>
2022-11-24 18:11:48 +0900

    del: add scene sheet

--- src/components/Layouts/NoPlusLayout.tsx
+++ src/components/Layouts/NoPlusLayout.tsx
@@ -1,55 +1,45 @@
 import { AppShell, Header, Stack, Title } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
 
-import { AddSceneSheet } from '~/components/BottomSheets/AddSceneSheet'
 import Footer from '~/components/Footer'
 import { HeadComponent } from '~/components/Head'
-import { TagUI } from '~/entities'
 
 type Props = {
   children: React.ReactNode
-  tags: TagUI[]
 }
 
-export const NoPlusLayout = ({ children, tags }: Props) => {
-  const [opened, handlers] = useDisclosure(false)
+export const NoPlusLayout = ({ children }: Props) => (
+  <AppShell
+    padding="md"
+    header={
+      <Header height={60} p="xs">
+        <Title order={1}>東海DB</Title>
+      </Header>
+    }
+    styles={(theme) => ({
+      main: {
+        backgroundColor: theme.colors.gray[0],
+        position: 'relative',
+      },
+    })}
+  >
+    <HeadComponent />
 
-  return (
-    <AppShell
-      padding="md"
-      header={
-        <Header height={60} p="xs">
-          <Title order={1}>東海DB</Title>
-        </Header>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor: theme.colors.gray[0],
-          position: 'relative',
-        },
-      })}
+    <Stack
+      align="center"
+      style={{
+        height: '100%',
+        minHeight: 'calc(100vh - 140px)',
+      }}
     >
-      <HeadComponent />
-
       <Stack
-        align="center"
         style={{
-          height: '100%',
-          minHeight: 'calc(100vh - 140px)',
+          maxWidth: 720,
         }}
       >
-        <Stack
-          style={{
-            maxWidth: 720,
-          }}
-        >
-          {children}
-        </Stack>
+        {children}
       </Stack>
+    </Stack>
 
-      <Footer />
-
-      <AddSceneSheet tags={tags} opened={opened} onClose={handlers.close} />
-    </AppShell>
-  )
-}
+    <Footer />
+  </AppShell>
+)
